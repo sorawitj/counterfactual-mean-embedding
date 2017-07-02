484140ee1c6ac1ec4086350a1fee74ac721c6e1a
krikamol <krikamol@gmail.com>
2017-07-02 16:00:39 +0700

    update the counterfactual mean estimator in Estimator.py

--- Simulation/simulation_runner.py
+++ Simulation/simulation_runner.py
@@ -16,7 +16,6 @@ def simmulate(policy, env, users):
     (reward, pick) = env.get_reward(context, reco)
     return {"x": context, "y": tuple(reco), "r": reward, "p": pick}
 
-
 def estimate(estimator, policy, sim_data):
     """
     estimate reward for a given policy and estimator
@@ -24,7 +23,6 @@ def estimate(estimator, policy, sim_data):
     :return: sum(reward)
     """
     return sum([estimator.estimate(d['x'], np.array(d['y']), d['r'], policy.recommend(d['x'])) for d in sim_data])
-
 
 def get_sim_reward(policy, env, users, N=10000):
     """
@@ -36,7 +34,6 @@ def get_sim_reward(policy, env, users, N=10000):
     for i in range(N):
         sim.append(simmulate(policy, env, users))
     return sum([d['r'] for d in sim])
-
 
 if __name__ == "__main__":
     config = {
@@ -53,7 +50,7 @@ if __name__ == "__main__":
                                       "actualVal"
                                       ])
 
-    # Score of user preferences (high score means user likes that item)
+    # score of user preferences (high score means user likes that item)
     user_score = [(u, np.random.normal(0, 1, config['n_items'])) for u in config['users']]
 
     # soft max distribution of items given the above score
@@ -80,7 +77,9 @@ if __name__ == "__main__":
         ipsEstimator = IPSEstimator(config['n_reco'], null_policy, new_policy)
         slateEstimator2 = SlateEstimatorImproved(config['n_reco'], null_policy)
 
-        # counterfactual mean estimator
+        # we instantiate the counterfactual mean estimator here. Note that we also pass the
+        # kernel functions (see scikit-learn doc: sklearn.metrics.pairwise) and their parameters.
+        
         #cmEstimator = CMEstimator(config['n_reco'], null_policy, sim_data, rbf_kernel, rbf_kernel, ...)
         
         directVal = estimate(directEstimator, new_policy, sim_data)
