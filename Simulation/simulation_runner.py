6b967cc9805e16c8ebf21cad1e7f0740e672db45
James S <sorawitj@gmail.com>
2017-10-23 15:52:19 +0700

    add temperature parameter to control action probabilities and set it to 1.5

--- Simulation/simulation_runner.py
+++ Simulation/simulation_runner.py
@@ -6,6 +6,7 @@ import numpy as np
 import pandas as pd
 from sklearn.metrics.pairwise import rbf_kernel
 
+
 def simulate_data(null_policy, new_policy, env, context, context_vectors, item_vectors, n_observation):
     """
     simulate data given policy, environment and set of context
@@ -26,7 +27,7 @@ def simulate_data(null_policy, new_policy, env, context, context_vectors, item_v
         new_reco = new_policy.recommend(pick_context)
         # recommendation is represented by an average over recommended item vectors
         new_reco_vec = np.mean(np.stack([item_vectors[r] for r in new_reco]), axis=0)
-        new_reward,_ = env.get_reward(pick_context, new_reco)
+        new_reward, _ = env.get_reward(pick_context, new_reco)
 
         observation = {"context": pick_context, "context_vec": context_vec, "null_reco": tuple(null_reco),
                        "null_reco_vec": null_reco_vec,
@@ -39,8 +40,8 @@ def simulate_data(null_policy, new_policy, env, context, context_vectors, item_v
 
     return sim_data
 
-def grid_search(params, estimator, sim_data, n_iterations):
 
+def grid_search(params, estimator, sim_data, n_iterations):
     actual_value = sim_data.new_reward.sum()
     return_df = pd.DataFrame(columns=['param', 'estimated_value', 'actual_value', 'error'])
     for param in params:
@@ -55,8 +56,9 @@ def grid_search(params, estimator, sim_data, n_iterations):
 
     return return_df
 
-def compare_estimators(estimators, n_iterations, null_policy, new_policy, environment, context_vectors, item_vectors, config):
-    
+
+def compare_estimators(estimators, n_iterations, null_policy, new_policy, environment, context_vectors, item_vectors,
+                       config):
     return_df = pd.DataFrame(columns=[e.name for e in estimators] + ['actual_value'])
     for i in range(n_iterations):
         sim_data = simulate_data(null_policy, new_policy, environment, config['context'], context_vectors, item_vectors,
@@ -68,14 +70,15 @@ def compare_estimators(estimators, n_iterations, null_policy, new_policy, enviro
 
     return return_df
 
-if __name__ == "__main__":
 
+if __name__ == "__main__":
     config = {
         "n_items": 6,
         "n_reco": 3,
         "n_observation": 5000,
         "context": ['A', 'B', 'C'],
-        "n_dim": 5
+        "n_dim": 5,
+        "tau": 1.5
     }
 
     # user features and item features, user preference = user_features.dot(item_features)
@@ -84,11 +87,13 @@ if __name__ == "__main__":
 
     # soft max distribution of items given the above score
     # this will be used in the Environment, then it is the optimal policy
-    context_prob = dict([(u, softmax(np.matmul(s, item_vectors.T))) for (u, s) in context_vectors.items()])
+    context_prob = dict([(u, softmax(np.matmul(s / config["tau"], item_vectors.T / config["tau"]))) for (u, s) in
+                         context_vectors.items()])
 
     # null policy is given by soft max distribution of items given negative of above score
     # this is an extreme case where null policy is totally opposite the optimal policy
-    null_policy_prob = dict([(u, softmax(-np.matmul(s, item_vectors.T))) for (u, s) in context_vectors.items()])
+    null_policy_prob = dict([(u, softmax(-np.matmul(s / config["tau"], item_vectors.T / config["tau"]))) for (u, s) in
+                             context_vectors.items()])
 
     null_policy = ProbPolicy(null_policy_prob, config['n_items'], config['n_reco'], greedy=False)
     # The policy we want to estimate is the optimal policy
@@ -114,10 +119,10 @@ if __name__ == "__main__":
     """
      Comparing between estimators
      """
-    #estimators = [DirectEstimator(), IPSEstimator(config['n_reco'], null_policy, new_policy),
+    # estimators = [DirectEstimator(), IPSEstimator(config['n_reco'], null_policy, new_policy),
     #           SlateEstimatorImproved(config['n_reco'], null_policy)]
 
-    #result_df = compare_estimators(estimators, 2, null_policy, new_policy, environment, context_vectors, item_vectors,
+    # result_df = compare_estimators(estimators, 10, null_policy, new_policy, environment, context_vectors, item_vectors,
     #                                config)
     #
     # result_df.plot.line(use_index=True)
