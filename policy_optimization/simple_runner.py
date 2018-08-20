e1efbc60e5ae157b7b67d19ecaf8ad06850d2751
Krikamol Muandet <krikamol@tuebingen.mpg.de>
2018-08-20 16:03:11 +0200

    update simple_runner.py

--- policy_optimization/simple_runner.py
+++ policy_optimization/simple_runner.py
@@ -1,16 +1,11 @@
+import sys
+sys.path.append("../policy_evaluation/")
+
 from CME import *
 from ParameterSelector import *
 from PolicyGradient import *
 import numpy as np
-
-config = {
-    "n_users": 10,
-    "n_items": 5,
-    "n_observation": 3000,
-    "context_dim": 10,
-    'learning_rate': 0.05
-}
-
+import matplotlib.pyplot as plt
 
 def get_sample_rewards(weight_vector, actions):
     return np.random.binomial(1, p=sigmoid(
@@ -21,47 +16,67 @@ def get_expected_reward(weight_vector, actions):
     p = sigmoid(sample_users[np.arange(len(sample_users)), actions, :].dot(weight_vector.T))
     return p.mean()
 
+# main
+if __name__=="__main__":
 
-user_item_vectors = np.random.normal(0, 1, size=(config['n_users'], config['n_items'], config['context_dim']))
+    config = {
+        "n_users": 10,
+        "n_items": 5,
+        "n_observation": 3000,
+        "context_dim": 10,
+        'learning_rate': 0.05
+    }
 
-sample_users = user_item_vectors[np.random.choice(user_item_vectors.shape[0], config['n_observation'], True), :]
+    SHOW_PLOT = True
+    num_iters = 1000
 
-true_weights = np.random.normal(0, 1, config['context_dim'])
+    user_item_vectors = np.random.normal(0, 1, size=(config['n_users'], config['n_items'], config['context_dim']))
 
-null_policy_weight = np.random.normal(0, 1, config['context_dim'])
+    sample_users = user_item_vectors[np.random.choice(user_item_vectors.shape[0], config['n_observation'], True), :]
 
-null_action_probs = softmax(sample_users.dot(null_policy_weight.T), axis=1)
+    print(np.shape(sample_users))
 
-null_actions = np.array(list(map(lambda x: np.random.choice(a=len(x), p=x), null_action_probs)))
+    true_weights = np.random.normal(0, 1, config['context_dim'])
+    null_policy_weight = np.random.normal(0, 1, config['context_dim'])
+    null_action_probs = softmax(sample_users.dot(null_policy_weight.T), axis=1)
+    null_actions = np.array(list(map(lambda x: np.random.choice(a=len(x), p=x), null_action_probs)))
+    null_rewards = get_sample_rewards(true_weights, null_actions)
 
-null_rewards = get_sample_rewards(true_weights, null_actions)
+    sess = tf.Session()
+    policy_grad = PolicyGradientAgent(config, sess)
+    sess.run(tf.global_variables_initializer())
 
-sess = tf.Session()
-policy_grad = PolicyGradientAgent(config, sess)
-sess.run(tf.global_variables_initializer())
+    null_feature_vec = sample_users[np.arange(len(sample_users)), null_actions, :]
+    cmeEstimator = CME(null_feature_vec, null_rewards)
 
-null_feature_vec = sample_users[np.arange(len(sample_users)), null_actions, :]
-cmeEstimator = CME(null_feature_vec, null_rewards)
+    loss_trace = np.zeros(num_iters)
+    
+    print("+--------------------------------------------------------------------------------------------------------+")
+    print("iter \t\t Exp Reward \t\t\t CME Reward \t\t\t Loss")
+    print("+--------------------------------------------------------------------------------------------------------+")
+    for i in range(num_iters):
+        
+        target_actions = policy_grad.act(sample_users)
+        target_feature_vec = sample_users[np.arange(len(sample_users)), target_actions, :]
+        target_reward = cmeEstimator.estimate(target_feature_vec)
 
-for i in range(200):
-    target_actions = policy_grad.act(sample_users)
+        loss = policy_grad.train_step(sample_users, null_actions, target_reward)
+        loss_trace[i] = loss
 
-    target_feature_vec = sample_users[np.arange(len(sample_users)), target_actions, :]
+        if i % 20 == 0:
+            print("{} \t\t {} \t\t {} \t\t {}".format(i, get_expected_reward(true_weights, target_actions),target_reward.sum(),loss))
 
-    target_reward = cmeEstimator.estimate(target_feature_vec)
+    print("+--------------------------------------------------------------------------------------------------------+")
 
-    loss = policy_grad.train_step(sample_users, null_actions, target_reward)
+    target_reward = get_expected_reward(true_weights, target_actions)
 
-    if i % 20 == 0:
-        print("iter {}, Expected reward: {}".format(i, get_expected_reward(true_weights, target_actions)))
-        print("iter {}, CME reward: {}".format(i, target_reward.sum()))
-        print("iter {}, loss: {}".format(i, target_reward.sum()))
+    optimal_actions = np.argmax(sample_users.dot(true_weights.T), axis=1)
+    optimal_rewards = get_expected_reward(true_weights, optimal_actions)
 
-print("FINISH TRAINING !!!")
-target_reward = get_expected_reward(true_weights, target_actions)
+    print("Target policy: Expected reward: {}".format(target_reward))
+    print("Optimal policy: Expected reward: {}".format(optimal_rewards))
 
-optimal_actions = np.argmax(sample_users.dot(true_weights.T), axis=1)
-optimal_rewards = get_expected_reward(true_weights, optimal_actions)
-
-print("Target policy: Expected reward: {}".format(target_reward))
-print("Optimal policy: Expected reward: {}".format(optimal_rewards))
+    if SHOW_PLOT:
+        # plot the trace
+        plt.plot(loss_trace)
+        plt.show()
